878fa35570f21b43b7ed6df258e98a31600afa90
Zakaria Azizi <azizi.zakaria@gmail.com>
2021-01-23 11:19:15 +0100

    add Configuration automatique

--- src/main/java/com/example/getway/GetwayApplication.java
+++ src/main/java/com/example/getway/GetwayApplication.java
@@ -2,8 +2,12 @@ package com.example.getway;
 
 
 
+import com.netflix.discovery.DiscoveryClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -15,13 +19,32 @@ public class GetwayApplication {
         SpringApplication.run(GetwayApplication.class, args);
     }
 
-    @Bean
+    // this is manual configuration has been disabled
+   // @Bean
     RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder){
         return  routeLocatorBuilder.routes()
 
-                .route((r)-> r.path("/personnas/**").uri("http://localhost:8081/"))
-                .route((r)-> r.path("/factoras/**").uri("http://localhost:8082/"))
+                // this is manual configuration
+
+                //.route((r)-> r.path("/personnas/**").uri("http://localhost:8081/"))
+                //.route((r)-> r.path("/factoras/**").uri("http://localhost:8082/"))
+               // .build();
+
+              //this configuration with leadBalancer
+                .route((r)-> r.path("/personnas/**").uri("lb://PERSONNA-SERVICE"))
+                .route((r)-> r.path("/factoras/**").uri("lb://FACTORA-SERVICE"))
                 .build();
+
     }
+
+   // this an automatique configuration by  the name in Client Url get mothde
+  @Bean
+  DiscoveryClientRouteDefinitionLocator definitionLocator
+          (ReactiveDiscoveryClient  discoveryClient, DiscoveryLocatorProperties properties ){
+     return  new  DiscoveryClientRouteDefinitionLocator(discoveryClient , properties);
+  }
+
+
+
 
 }
